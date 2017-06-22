a2f577242df781a0e479359d4ab90b82a2220e0a
Connor Dittmar <connor.dittmar@gmail.com>
2017-06-22 15:29:28 -0400

    pushed target template

--- vision/views.py
+++ vision/views.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
 from .models import Target
 
 def index(request):
@@ -12,7 +11,7 @@ def target_view(request, target_id):
     return HttpResponse("Looking at target %s." % target_id)
 
 def targets(request):
-    target_list = Target.objects.order_by('-id')
-    output = "Targets Overview: " + ', '.join([t.target_type for t in target_list])
-    return HttpResponse(output)
+    targets_list = Target.objects.order_by('-id')
+    context = {'targets_list': targets_list}
+    return render(request, 'vision/targets.html', context)
 # Create your views here.
