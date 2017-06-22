aa10e062c06867d39887d102bccc48a9c1e9fcd8
Connor Dittmar <connor.dittmar@gmail.com>
2017-06-22 15:51:06 -0400

    added target-specific page template

--- vision/urls.py
+++ vision/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'targets/$', views.targets, name='targets'),
-    url(r'targets/(?P<target_id>[0-9]+)/$', views.target_view, name = 'target_view')
+    url(r'targets/(?P<target_id>[0-9]+)/$', views.target_view, name = 'target_view'),
+    url(r'axiscam/$', views.axis_cam_view, name='axis_cam_view')
 ]
--- vision/views.py
+++ vision/views.py
@@ -8,10 +8,15 @@ def index(request):
     return render(request, 'vision/index.html')
 
 def target_view(request, target_id):
-    return HttpResponse("Looking at target %s." % target_id)
+    target = Target.objects.get(id=target_id)
+    context = {'target': target}
+    return render(request, 'vision/single_target.html', context)
 
 def targets(request):
     targets_list = Target.objects.order_by('-id')
     context = {'targets_list': targets_list}
     return render(request, 'vision/targets.html', context)
+
+def axis_cam_view(request):
+    return render(request, 'vision/axiscam.html')
 # Create your views here.
